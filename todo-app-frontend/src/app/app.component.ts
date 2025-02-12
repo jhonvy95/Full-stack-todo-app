c7b2abd9a1920d72db70ee852fd5c45a75db08ea
jhon <jyate@smartsoft.la>
2025-02-11 23:25:23 -0500

    Initialized Angular project with base structure,global styles, and necessary dependencies.

---
+++ todo-app-frontend/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [UserComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'todo-app-frontend';
+}
