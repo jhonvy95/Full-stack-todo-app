449e3f062d1787c2128cf7ece7ebcfd94d69f311
jhon <jyate@smartsoft.la>
2025-02-12 20:59:58 -0500

    Implement Search and Filter Functionality for Task List

--- todo-app-frontend/src/app/tasks/tasks.component.ts
+++ todo-app-frontend/src/app/tasks/tasks.component.ts
@@ -4,15 +4,28 @@ import { TaskService } from '../core/services/task.service';
 import { MatDialog } from '@angular/material/dialog';
 import { TodoFormDialogComponent } from '../shared/components/todo-form-dialog/todo-form-dialog.component';
 import { TaskListComponent } from './task-list/task-list.component';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-tasks',
-  imports: [TaskListComponent],
+  imports: [
+    TaskListComponent,
+    MatFormField,
+    MatLabel,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
   tasks: Task[] = [];
+  filteredTasks: Task[] = [];
+  filterText: string = '';
 
   constructor(private taskService: TaskService, private dialog: MatDialog) {}
 
@@ -34,6 +47,18 @@ export class TasksComponent {
   refreshTasks(): void {
     this.taskService.getTasks().subscribe((tasks) => {
       this.tasks = tasks;
+      this.applyFilter();
     });
+  }
+
+  onFilterChange(): void {
+    this.applyFilter();
+  }
+
+  applyFilter(): void {
+    const filter = this.filterText.toLowerCase().trim();
+    this.filteredTasks = this.tasks.filter((task) =>
+      task.title.toLowerCase().includes(filter)
+    );
   }
 }
